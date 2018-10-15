c1577c77563f200497283df41a3d2b0ec2d91bd8
Todd Schueren <19.todd.schueren@ksd.org>
2018-10-15 09:10:02 -0700

    tried dashing :(

--- Tower/Assets/Script/movement.cs
+++ Tower/Assets/Script/movement.cs
@@ -5,13 +5,30 @@ using UnityEngine;
 public class movement : MonoBehaviour {
 
     public float speed = 3.0f;
+    public float dashSpeed = 5.0f;
+    float timer = 0.2f;
+    public float StartSpeed = 3.0f;
 
-	
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update () {
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
         Vector2 velocity = new Vector2(x, y);
         GetComponent<Rigidbody2D>().velocity = velocity * speed ;
-	}
+        if (Input.GetKeyDown(KeyCode.Space) && timer > 0.2f)
+        {
+            timer = 0;
+
+        }
+        if (timer < 0.5f)
+        {
+            //increase speed
+            
+            
+        }
+        else
+        {
+            //reset speed
+        }
+    }
 }
