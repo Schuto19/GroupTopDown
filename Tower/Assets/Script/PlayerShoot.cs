8f9e076e2ddbcefe3dc3f51e8308a95982ac0099
Todd Schueren <19.todd.schueren@ksd.org>
2018-10-16 10:17:57 -0700

    Mp

--- Tower/Assets/Script/PlayerShoot.cs
+++ Tower/Assets/Script/PlayerShoot.cs
@@ -1,22 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerShoot : MonoBehaviour {
 
     public GameObject prefab;
     public float shootSpeed = 10;
     float timer = 0;
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public int Mp = 10;
+    public Text MpText;
+    public Slider MpBar;
+
+    // Use this for initialization
+    void Start () {
+        MpText.GetComponent<Text>().text = "MP: " + Mp;
+        MpBar.GetComponent<Slider>().value = Mp;
+    }
 	
 	// Update is called once per frame
 	void Update () {
         timer += Time.deltaTime;
-        if (Input.GetButton("Fire2") && timer > 0.5f)
+        if (Input.GetButton("Fire2") && timer > 0.5f && Mp > 0)
         {
+            Mp--;
+            MpBar.GetComponent<Slider>().value = Mp;
+            MpText.GetComponent<Text>().text = "MP: " + Mp;
             timer = 0;
             var mousePosition = Input.mousePosition;
             mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
@@ -28,6 +37,16 @@ public class PlayerShoot : MonoBehaviour {
             GameObject bullet = (GameObject)Instantiate(prefab, transform.position, Quaternion.identity);
             bullet.GetComponent<Rigidbody2D>().velocity = shootDir;
             Destroy(bullet, 1.0f);
+            
         }
 	}
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "MpStar")
+        {
+            Mp++;
+            MpText.GetComponent<Text>().text = "MP: " + Mp;
+            Destroy(collision.gameObject);
+        }
+    }
 }
